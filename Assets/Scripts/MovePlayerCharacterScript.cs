849669f6a348138352b4e702a7d2f05390ac6bed
AKing58 <35930720+AKing58@users.noreply.github.com>
2019-03-06 21:39:23 -0800

    Movent is working now kinda

---
+++ Assets/Scripts/MovePlayerCharacterScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayerCharacterScript : MonoBehaviour
+{
+
+    public string entityPosition;
+
+    public float locPosx;
+    public float locPosy;
+    public float locPosz;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        entityPosition = "front";
+        locPosx = transform.localPosition.x;
+        locPosy = transform.localPosition.y;
+        locPosz = transform.localPosition.z;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+    }
+
+    public void moveCharacter()
+    {
+        if(entityPosition == "front")
+        {
+            transform.localPosition = new Vector3(locPosx, locPosy, locPosz-(float)1.5);
+            entityPosition = "back";
+        }
+        else
+        {
+            transform.localPosition = new Vector3(locPosx, locPosy, locPosz);
+            entityPosition = "front";
+        }
+        
+    }
+}
